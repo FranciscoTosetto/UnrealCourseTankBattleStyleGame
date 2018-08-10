cc6d034cb2f1fa4c8b8ed9570e9e1f817f2a0163
LAPTOP-9FMO7MCF\Franc <franciscotosetto@gmail.com>
2018-08-10 16:48:57 -0300

    Tank Battle Style Game c.01 (Created project and learning about landscape)

---
+++ TankBattleStyleGame/Source/TankBattleStyleGame/TankBattleStyleGame.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankBattleStyleGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TankBattleStyleGame, "TankBattleStyleGame" );
